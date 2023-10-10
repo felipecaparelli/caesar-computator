e11d077e4aa461cc2a0b4cb0ab050eeab4054442
Felipe Caparelli <felipecaparelli@gmail.com>
2023-10-10 12:10:02 +0200

    KT-0002: Updating unit tests

--- src/test/kotlin/com/caparelli/MainKtTest.kt
+++ src/test/kotlin/com/caparelli/MainKtTest.kt
@@ -1,28 +1,52 @@
 package com.caparelli
 
 import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
 
 import kotlin.test.assertFailsWith
 
 internal class MainKtTest {
 
     @Test
-    fun convertIntToRomanWhenParamIsNull() {
-        assertFailsWith<IllegalArgumentException>(
-            message = "Invalid input (null). Please enter a valid integer.",
-            block = {
-                convertIntToRoman(null)
-            }
-        )
+    fun convertIntToRoman() {
+        assertEquals("54 in Roman numerals is: LIV", convertToRomanOrInteger("54"))
+        assertEquals("129 in Roman numerals is: CXXIX", convertToRomanOrInteger("129"))
+        assertEquals("587 in Roman numerals is: DLXXXVII", convertToRomanOrInteger("587"))
+    }
+
+    @Test
+    fun convertRomanToInt() {
+        assertEquals("V in integer form is: 5", convertToRomanOrInteger("V"))
+        assertEquals("XXIII in integer form is: 23", convertToRomanOrInteger("XXIII"))
+        assertEquals("MCMLXXXII in integer form is: 1982", convertToRomanOrInteger("MCMLXXXII"))
+    }
+
+    @Test
+    fun convertIntToRomanWhenParamIsEmpty() {
+        assertFailsWith<IllegalArgumentException> {
+            convertToRomanOrInteger(null)
+        }
     }
 
     @Test
     fun convertIntToRomanWhenParamIsInvalid() {
-        assertFailsWith<IllegalArgumentException>(
-            message = "Invalid input. Please enter a valid integer.",
-            block = {
-                convertIntToRoman("AA")
-            }
-        )
+        assertFailsWith<IllegalArgumentException> {
+            convertToRomanOrInteger("DAX")
+        }
     }
+
+    @Test
+    fun convertIntToRomanWhenParamIsInvalid2() {
+        assertFailsWith<IllegalArgumentException> {
+            convertToRomanOrInteger("**")
+        }
+    }
+
+    @Test
+    fun convertIntToRomanWhenParamIsInvalidInteger() {
+        assertFailsWith<IllegalArgumentException> {
+            convertToRomanOrInteger("-10")
+        }
+    }
+
 }
--- src/test/kotlin/com/caparelli/RomanConverterTest.kt
+++ src/test/kotlin/com/caparelli/RomanConverterTest.kt
@@ -2,6 +2,7 @@ package com.caparelli
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import kotlin.test.assertTrue
 
 internal class RomanConverterTest {
 
@@ -15,5 +16,19 @@ internal class RomanConverterTest {
     @Test
     fun testIntToRomanInvalidInput() {
         assertEquals("", RomanConverter().intToRoman(-1))
+    }
+
+    @Test
+    fun testRomanToInt() {
+        assertEquals(4, RomanConverter().romanToInt("IV"))
+        assertEquals(6, RomanConverter().romanToInt("VI"))
+        assertEquals(9, RomanConverter().romanToInt("IX"))
+        assertEquals(12, RomanConverter().romanToInt("XII"))
+    }
+
+    @Test
+    fun isRomanNumeral() {
+        assertTrue(RomanConverter().isRomanNumeral("XVI"))
+        assertTrue(RomanConverter().isRomanNumeral("D"))
     }
 }
